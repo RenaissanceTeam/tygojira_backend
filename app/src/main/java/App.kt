824fab96284a653f334d5a54af40a3773189392b
Nikolay Skudniakov <nikolay.skudniakov@hiq.ru>
2020-01-05 16:33:42 +0300

    Add auth modules

--- app/src/main/java/App.kt
+++ app/src/main/java/App.kt
@@ -1,5 +1,3 @@
-package ru.fors
-
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.boot.autoconfigure.SpringBootApplication
