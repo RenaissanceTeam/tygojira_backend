ce597b4e8d23a788e7ad84e405449c9376be8566
b1nd <h33el@yandex.ru>
2020-01-08 16:51:20 +0300

    pr #3 #4 fixes add logging to file

--- auth/src/main/java/ru/fors/auth/data/UserRoleRepo.kt
+++ auth/src/main/java/ru/fors/auth/data/UserRoleRepo.kt
@@ -1,7 +1,6 @@
 package ru.fors.auth.data
 
 import org.springframework.data.jpa.repository.JpaRepository
-import ru.fors.auth.domain.entity.User
 import ru.fors.auth.domain.entity.UserRole
 
 interface UserRoleRepo : JpaRepository<UserRole, String> {
