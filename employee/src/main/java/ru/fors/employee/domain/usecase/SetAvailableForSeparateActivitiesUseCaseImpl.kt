ea743c23daa049feee1a5b6cd868e9cd7ca57024
Nikolay Skudniakov <nikolay.skudniakov@hiq.ru>
2020-02-21 10:47:00 +0300

    Restrict setting days to pr only

--- employee/src/main/java/ru/fors/employee/domain/usecase/SetAvailableForSeparateActivitiesUseCaseImpl.kt
+++ employee/src/main/java/ru/fors/employee/domain/usecase/SetAvailableForSeparateActivitiesUseCaseImpl.kt
@@ -1,19 +1,25 @@
 package ru.fors.employee.domain.usecase
 
 import org.springframework.stereotype.Component
+import ru.fors.auth.api.domain.RoleChecker
+import ru.fors.auth.api.domain.requireOne
 import ru.fors.employee.api.domain.usecase.GetEmployeeByIdUseCase
 import ru.fors.employee.api.domain.usecase.SetAvailableForSeparateActivitiesUseCase
 import ru.fors.employee.data.repo.SeparateActivityAvailabilityRepo
+import ru.fors.entity.employee.Role
 import ru.fors.entity.employee.SeparateActivityAvailability
 import java.util.*
 
 @Component
 class SetAvailableForSeparateActivitiesUseCaseImpl(
         private val repo: SeparateActivityAvailabilityRepo,
-        private val getEmployeeUseCase: GetEmployeeByIdUseCase
+        private val getEmployeeUseCase: GetEmployeeByIdUseCase,
+        private val roleChecker: RoleChecker
 ) : SetAvailableForSeparateActivitiesUseCase {
 
     override fun execute(id: Long, available: List<Date>): SeparateActivityAvailability {
+        roleChecker.requireOne(Role.PROJECT_LEAD)
+
         val availability = repo.findByEmployeeId(id) ?: createAvailability(id)
         return repo.save(availability.copy(dates = available))
     }
