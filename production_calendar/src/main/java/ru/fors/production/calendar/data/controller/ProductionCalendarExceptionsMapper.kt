07625d229f0fec0150afe59538f76bead1604b44
makar <zlysenko.makarz@gmail.com>
2020-02-24 17:47:39 +0300

    add  production calendar exceptions mapper

---
+++ production_calendar/src/main/java/ru/fors/production/calendar/data/controller/ProductionCalendarExceptionsMapper.kt
@@ -0,0 +1,14 @@
+package ru.fors.production.calendar.data.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import ru.fors.production.calendar.api.domain.entity.HolidayNotFoundException
+import ru.fors.util.extensions.toResponseEntityStatus
+
+@ControllerAdvice
+class ProductionCalendarExceptionsMapper {
+
+    @ExceptionHandler
+    fun noEmployee(e: HolidayNotFoundException) = e.toResponseEntityStatus(HttpStatus.NOT_FOUND)
+}
