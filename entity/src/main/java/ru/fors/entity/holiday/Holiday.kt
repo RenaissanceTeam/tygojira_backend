374a973e813e45fe0473c901facc7918157e0daf
makar <zlysenko.makarz@gmail.com>
2020-02-24 15:36:29 +0300

    impl production calendar controller

--- entity/src/main/java/ru/fors/entity/holiday/Holiday.kt
+++ entity/src/main/java/ru/fors/entity/holiday/Holiday.kt
@@ -1,8 +1,10 @@
 package ru.fors.entity.holiday
 
 import java.time.LocalDate
+import javax.persistence.Entity
 import javax.persistence.Id
 
+@Entity
 data class Holiday(
         val title: String,
         val description: String,
