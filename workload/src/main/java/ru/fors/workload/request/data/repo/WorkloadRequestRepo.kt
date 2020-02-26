7fd6c5b4218bc7cd9207c58ffd4c593cd498d258
Nick Skudnyakov <helloma@yandex.ru>
2020-02-26 15:47:15 +0300

    Update workload/src/main/java/ru/fors/workload/request/data/repo/WorkloadRequestRepo.kt

--- workload/src/main/java/ru/fors/workload/request/data/repo/WorkloadRequestRepo.kt
+++ workload/src/main/java/ru/fors/workload/request/data/repo/WorkloadRequestRepo.kt
@@ -1,7 +1,6 @@
 package ru.fors.workload.request.data.repo
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import ru.fors.entity.employee.Employee
 import ru.fors.entity.workload.request.WorkloadRequest
 
