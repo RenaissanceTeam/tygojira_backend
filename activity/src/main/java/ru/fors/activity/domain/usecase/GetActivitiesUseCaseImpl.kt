e3f9ce65c5afa25c36a46b8715c84681986ae2d0
makar <zlysenko.makarz@gmail.com>
2020-03-29 15:00:03 +0300

    filter get activities by business role

--- activity/src/main/java/ru/fors/activity/domain/usecase/GetActivitiesUseCaseImpl.kt
+++ activity/src/main/java/ru/fors/activity/domain/usecase/GetActivitiesUseCaseImpl.kt
@@ -3,17 +3,34 @@ package ru.fors.activity.domain.usecase
 import org.springframework.stereotype.Component
 import ru.fors.activity.api.domain.usecase.GetActivitiesUseCase
 import ru.fors.activity.data.repo.ActivityRepo
+import ru.fors.employee.api.domain.usecase.CheckUserHasBusinessRoleUseCase
+import ru.fors.employee.api.domain.usecase.GetCallingEmployeeUseCase
 import ru.fors.entity.activity.Activity
+import ru.fors.entity.employee.Role
 import ru.fors.pagination.api.domain.entity.Page
 import ru.fors.pagination.api.domain.entity.PageRequest
 import ru.fors.util.extensions.toPage
 import ru.fors.util.extensions.toSpringPageRequest
+import ru.fors.workload.api.domain.usecase.GetActivitiesAndWorkloadsForEmployeeUseCase
 
 @Component
-class GetActivitiesUseCaseImpl (
+class GetActivitiesUseCaseImpl(
+        private val checkUserHasBusinessRoleUseCase: CheckUserHasBusinessRoleUseCase,
+        private val getCallingEmployeeUseCase: GetCallingEmployeeUseCase,
+        private val getActivitiesAndWorkloadsForEmployeeUseCase: GetActivitiesAndWorkloadsForEmployeeUseCase,
         private val repo: ActivityRepo
-): GetActivitiesUseCase {
+) : GetActivitiesUseCase {
     override fun execute(pageRequest: PageRequest): Page<Activity> {
-        return repo.findAll(pageRequest.toSpringPageRequest()).toPage()
+        val caller = getCallingEmployeeUseCase.execute()
+        return when {
+            checkUserHasBusinessRoleUseCase.execute(Role.PROJECT_OFFICE) -> repo.findAll(pageRequest.toSpringPageRequest())
+            checkUserHasBusinessRoleUseCase.execute(Role.LINEAR_LEAD) -> repo.findAllByLeadSubdivision(caller.subdivision, pageRequest.toSpringPageRequest())
+            checkUserHasBusinessRoleUseCase.execute(Role.PROJECT_LEAD) -> repo.findAllByLead(caller, pageRequest.toSpringPageRequest())
+            checkUserHasBusinessRoleUseCase.execute(Role.EMPLOYEE) -> {
+                val callerActivities = getActivitiesAndWorkloadsForEmployeeUseCase.execute(caller.id).map { it.activity.id }
+                repo.findAllByIdIn(callerActivities, pageRequest.toSpringPageRequest())
+            }
+            else -> throw IllegalArgumentException("Unknown business role")
+        }.toPage()
     }
 }
